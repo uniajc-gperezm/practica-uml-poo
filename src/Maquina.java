0ad0b7ed4e6250cbeb88327095aeeb13f638fee7
saantiagg <santimunoz68@gmail.com>
2025-08-22 20:46:01 -0500

    Primer cambioo

---
+++ src/Maquina.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+    public class Maquina {
+
+        public static void main(String[] args) {
+            
+        System.out.println("Hola Santiago");
+        }
+    
+}
